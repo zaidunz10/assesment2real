518e9e8997ce50bfbfd55b8bfbf5436a8308630e
zaidunz10 <zid4nsyhid@gmail.com>
2026-05-20 01:57:14 +0700

    add package

--- app/src/main/java/com/zidansyahidagrifasa0072/assesment2/MainActivity.kt
+++ app/src/main/java/com/zidansyahidagrifasa0072/assesment2/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assesment2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
